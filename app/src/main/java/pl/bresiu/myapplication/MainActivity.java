0fe4c86c07f9b20e6458aa632603e74fa5a4ddf0
Bresiu <michal.brewczak@gmail.com>
2015-09-15 16:48:12 +0200

    first commit

---
+++ app/src/main/java/pl/bresiu/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package pl.bresiu.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by Bresiu on 15-09-2015
+ */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
